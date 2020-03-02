d2e8e82fc33291000af780773d711bc25406547d
bbashz10 <bbashz10@localhost.localdomain>
2020-03-02 11:53:58 +0530

    heroku added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
 
 const app = express()
+const port = process.env.PORT || 3000       //config port for heroku
 const publicDirectory = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')        //when changing the views name
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -118,6 +119,6 @@ app.get('*', (req, res) => {
     res.send('my 404 page')
 })
 
-app.listen(3000, () => {
-    console.log('Server is up for port 3000')
+app.listen(port, () => {
+    console.log('Server is up for port ' + port)
 })
